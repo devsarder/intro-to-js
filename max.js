ab6be94a84a277a446431d73524976c2531e16c1
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-08-06 16:07:35 +0200

    utilized Math.max() to find the max number

---
+++ max.js
@@ -0,0 +1,41 @@
+// let jim = 90;
+// let dela = 87;
+// let chinku = 77;
+
+// if(jim > dela && jim >chinku){
+//     console.log('jim you will get the cake');
+// }
+// else if(dela > jim && dela > chinku){
+//     console.log('dela you will get the cake');
+// }
+// else{
+//     console.log('chinku you will get the cake');
+// }
+
+function determineCakeRecipient(jim,dela,chinku){
+    if(jim > dela && jim > chinku){
+        return 'jim';
+    }
+    else if(dela > jim && dela > chinku){
+        return 'dela';
+    }
+    else{
+        return 'chinku';
+    }
+}
+let jim = 58;
+let dela = 87;
+let chinku = 99;
+const recipient = determineCakeRecipient(jim,dela,chinku);
+console.log(recipient,'you will get the cake');
+
+
+function maxNum(a,b,c){
+    return Math.max(a,b,c);
+}
+let a = 100;
+let b = 200;
+let c = 300;
+const maxNumber = maxNum(a,b,c);
+console.log('the max number is :', maxNumber);
+
