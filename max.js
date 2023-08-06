b333e742eaaf26685f69aa52f7d7b3aeffa0f8db
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-08-06 16:39:45 +0200

    found the tallest person

--- max.js
+++ max.js
@@ -12,30 +12,46 @@
 //     console.log('chinku you will get the cake');
 // }
 
-function determineCakeRecipient(jim,dela,chinku){
-    if(jim > dela && jim > chinku){
-        return 'jim';
-    }
-    else if(dela > jim && dela > chinku){
-        return 'dela';
-    }
-    else{
-        return 'chinku';
-    }
+function determineCakeRecipient(jim, dela, chinku) {
+  if (jim > dela && jim > chinku) {
+    return "jim";
+  } else if (dela > jim && dela > chinku) {
+    return "dela";
+  } else {
+    return "chinku";
+  }
 }
 let jim = 58;
 let dela = 87;
 let chinku = 99;
-const recipient = determineCakeRecipient(jim,dela,chinku);
-console.log(recipient,'you will get the cake');
+const recipient = determineCakeRecipient(jim, dela, chinku);
+console.log(recipient, "you will get the cake");
 
-
-function maxNum(a,b,c){
-    return Math.max(a,b,c);
+function maxNum(a, b, c) {
+  return Math.max(a, b, c);
 }
 let a = 100;
 let b = 200;
 let c = 300;
-const maxNumber = maxNum(a,b,c);
-console.log('the max number is :', maxNumber);
+const maxNumber = maxNum(a, b, c);
+console.log("the max number is :", maxNumber);
+
+// used function to find the tallest number of an array..
+
+function maxInArray(numbers) {
+  let tallest = [0];
+  for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    if(element > tallest){
+        tallest = element;
+    }
+  }
+  return tallest;
+   
+}
+
+let heights = [165, 265, 465, 789, 152, 133];
+const tallestPerson = maxInArray(heights);
+console.log ('the tallest person is ',tallestPerson);
+
 
